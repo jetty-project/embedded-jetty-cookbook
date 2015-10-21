aec42b992273f07cf1445358da6fcc5a29d61348
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2015-10-21 11:28:19 -0700

    Adding GzipResponseExample (for GzipHandler)

---
+++ src/main/java/org/eclipse/jetty/cookbook/GzipResponsesExample.java
@@ -0,0 +1,53 @@
+package org.eclipse.jetty.cookbook;
+
+import java.io.File;
+import java.nio.file.Path;
+
+import javax.websocket.server.ServerContainer;
+
+import org.eclipse.jetty.cookbook.servlets.TimeServlet;
+import org.eclipse.jetty.cookbook.websocket.jsr.TimeSocket;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.handler.gzip.GzipHandler;
+import org.eclipse.jetty.servlet.DefaultServlet;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.util.resource.PathResource;
+import org.eclipse.jetty.websocket.jsr356.server.deploy.WebSocketServerContainerInitializer;
+
+public class GzipResponsesExample
+{
+    public static void main(String[] args) throws Exception
+    {
+        Server server = new Server();
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8080);
+        server.addConnector(connector);
+
+        GzipHandler gzip = new GzipHandler();
+        gzip.setIncludedMethods("GET","POST");
+        gzip.setMinGzipSize(245);
+        gzip.setIncludedMimeTypes("text/plain","text/css","text/html",
+                "application/javascript");
+        server.setHandler(gzip);
+
+        Path webRootPath = new File("webapps/static-root/").toPath().toRealPath();
+
+        ServletContextHandler context = new ServletContextHandler();
+        gzip.setHandler(context);
+        context.setContextPath("/");
+        context.setBaseResource(new PathResource(webRootPath));
+        context.setWelcomeFiles(new String[] { "index.html" });
+        
+        // Adding WebSockets
+        ServerContainer wsContainer = WebSocketServerContainerInitializer.configureContext(context);
+        wsContainer.addEndpoint(TimeSocket.class);
+
+        // Adding Servlets
+        context.addServlet(TimeServlet.class,"/time/");
+        context.addServlet(DefaultServlet.class,"/"); // always last, always on "/"
+
+        server.start();
+        server.join();
+    }
+}
---
+++ src/main/java/org/eclipse/jetty/cookbook/servlets/TimeServlet.java
@@ -0,0 +1,29 @@
+package org.eclipse.jetty.cookbook.servlets;
+
+import java.io.IOException;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class TimeServlet extends HttpServlet
+{
+    private static final TimeZone TZ = TimeZone.getDefault();
+
+    @Override
+    public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
+    {
+        Locale locale = req.getLocale();
+        Calendar cal = Calendar.getInstance(TZ,locale);
+        String dateStr = DateFormat.getDateInstance(DateFormat.DEFAULT,locale).format(cal.getTime());
+        String timeStr = DateFormat.getTimeInstance(DateFormat.DEFAULT,locale).format(cal.getTime());
+        String tzStr = TZ.getDisplayName(false,TimeZone.SHORT,locale);
+        resp.getWriter().println(String.format("%s %s %s",dateStr,timeStr,tzStr));
+    }
+}
