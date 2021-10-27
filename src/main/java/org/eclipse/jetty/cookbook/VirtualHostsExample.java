9de23a056ec3d4ee5f364cb4a16bd576683ddfba
Joakim Erdfelt <joakim.erdfelt@gmail.com>
2021-10-27 10:36:21 -0500

    Fixing VirtualHostsExample
    Signed-off-by: Joakim Erdfelt <joakim.erdfelt@gmail.com>

--- src/main/java/org/eclipse/jetty/cookbook/VirtualHostsExample.java
+++ src/main/java/org/eclipse/jetty/cookbook/VirtualHostsExample.java
@@ -16,7 +16,6 @@ package org.eclipse.jetty.cookbook;
 import java.io.IOException;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -26,37 +25,31 @@ import org.eclipse.jetty.server.handler.ContextHandler;
 import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
 import org.eclipse.jetty.util.IO;
+import org.eclipse.jetty.util.component.LifeCycle;
 
-@SuppressWarnings("Duplicates")
 public class VirtualHostsExample
 {
-    public static void main(String[] args)
+    public static void main(String[] args) throws Exception
     {
         VirtualHostsExample example = new VirtualHostsExample();
         try
         {
             example.startServer();
-            example.testRequest("a.company.com","/hello");
-            example.testRequest("b.company.com","/hello");
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
+            example.testRequest("foo.company.com", "/hello");
+            example.testRequest("bar.company.com", "/hello");
         }
         finally
         {
             example.stopServer();
         }
     }
-    
+
     private Server server;
 
     private void stopServer()
     {
-        try { server.stop(); }
-        catch (Exception ignore) { }
+        LifeCycle.stop(server);
     }
 
     private void startServer() throws Exception
@@ -64,41 +57,31 @@ public class VirtualHostsExample
         server = new Server(8080);
         HandlerCollection handlers = new HandlerCollection();
         server.setHandler(handlers);
-        
-        handlers.addHandler(createContext("/", "a.company.com"));
-        handlers.addHandler(createContext("/", "b.company.com"));
-        
+
+        handlers.addHandler(createContext("/", "foo.company.com", HelloFooServlet.class));
+        handlers.addHandler(createContext("/", "bar.company.com", HelloBarServlet.class));
+
         server.start();
     }
 
-    private ContextHandler createContext(String contextPath, final String host)
+    private ContextHandler createContext(String contextPath, String host, Class<? extends HttpServlet> virtualHostSpecificServlet)
     {
         ServletContextHandler context = new ServletContextHandler();
         context.setContextPath(contextPath);
-        @SuppressWarnings("serial")
-        ServletHolder helloholder = new ServletHolder(new HttpServlet()
-        {
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-            {
-                resp.setContentType("text/plain");
-                resp.getWriter().printf("Hello from [%s] context%n",host);
-            }
-        });
-        context.addServlet(helloholder, "/hello");
-        context.addServlet(DefaultServlet.class,"/");
-        ContextHandler vhwrapper = new ContextHandler();
-        vhwrapper.setHandler(context);
-        vhwrapper.setVirtualHosts(new String[]{host});
-        return vhwrapper;
+
+        context.addServlet(virtualHostSpecificServlet, "/hello");
+        context.addServlet(DefaultServlet.class, "/");
+
+        context.setVirtualHosts(new String[]{host});
+        return context;
     }
 
     private void testRequest(String host, String path)
     {
-        try(Socket client = new Socket("localhost",8080);)
+        try (Socket client = new Socket("localhost", 8080);)
         {
-            System.out.printf("-- testRequest [%s] [%s] --%n",host,path);
-            String req = String.format("GET %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n",path,host);
+            System.out.printf("%n-- testRequest [%s] [%s] --%n", host, path);
+            String req = String.format("GET %s HTTP/1.1\r\nHost: %s\r\nConnection: close\r\n\r\n", path, host);
             System.out.print(req);
             client.getOutputStream().write(req.getBytes(StandardCharsets.UTF_8));
             String response = IO.toString(client.getInputStream());
@@ -107,6 +90,28 @@ public class VirtualHostsExample
         catch (Exception e)
         {
             e.printStackTrace();
+        }
+    }
+
+    public static class HelloFooServlet extends HttpServlet
+    {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
+        {
+            resp.setCharacterEncoding("utf-8");
+            resp.setContentType("text/plain");
+            resp.getWriter().println("Greetings from [HelloFooServlet]");
+        }
+    }
+
+    public static class HelloBarServlet extends HttpServlet
+    {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
+        {
+            resp.setCharacterEncoding("utf-8");
+            resp.setContentType("text/plain");
+            resp.getWriter().println("Salutations from [HelloBarServlet]");
         }
     }
 }
